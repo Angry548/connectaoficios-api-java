05e7d21f8706277fdcab7d46621ce893bcfcefe9
Angry548 <angelmate827@gmail.com>
2026-09-20 14:24:28 -0600

    feat(domain): [CON-78] Jose Angel Mate Carias - creación del enum TipoNotificacion
    Se definió la enumeración TipoNotificacion para clasificar los tipos de notificaciones en el dominio.

---
+++ src/main/java/com/connectaoficios/api/enums/TipoNotificacion.java
@@ -0,0 +1,11 @@
+package com.connectaoficios.api.enums;
+
+public enum TipoNotificacion {
+
+    NUEVA_SOLICITUD,
+    CAMBIO_ESTADO_SOLICITUD,
+    NUEVO_MENSAJE,
+    NUEVA_RESENA,
+    PROMOCION,
+    SISTEMA
+}
