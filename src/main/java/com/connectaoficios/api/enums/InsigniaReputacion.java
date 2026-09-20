c464000fab97a961baa1e4916f408719119861f8
Angry548 <angelmate827@gmail.com>
2026-09-20 14:41:02 -0600

    feat(domain): [CON-70] Jose Angel Mate Carias - creación del enum InsigniaReputacion
    Se definió la enumeración InsigniaReputacion para clasificar las insignias de reputación en el dominio.

--- src/main/java/com/connectaoficios/api/enums/InsigniaReputacion.java
+++ src/main/java/com/connectaoficios/api/enums/InsigniaReputacion.java
@@ -1,4 +1,9 @@
 package com.connectaoficios.api.enums;
 
-public class InsigniaReputacion {
+public enum InsigniaReputacion {
+
+    NUEVO_TRABAJADOR,
+    TRABAJADOR_CONFIABLE,
+    MEJOR_VALORADO,
+    TOP_PLATAFORMA
 }
