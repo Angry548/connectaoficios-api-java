66ab427209128e27581d1b9f0fbf133eabdfd19c
Angry548 <angelmate827@gmail.com>
2026-09-20 15:39:02 -0600

    feat(domain): [CON-64] Jose Angel Mate Carias - creación del modelo PlanPromocion
    Se definió la entidad PlanPromocion para la gestión y configuración de los planes promocionales en el dominio.

--- src/main/java/com/connectaoficios/api/modelos/PlanPromocion.java
+++ src/main/java/com/connectaoficios/api/modelos/PlanPromocion.java
@@ -1,4 +1,37 @@
 package com.connectaoficios.api.modelos;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "planes_promocion")
 public class PlanPromocion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100, unique = true)
+    private String nombre;
+
+    @Column(length = 500)
+    private String descripcion;
+
+    @Column(name = "duracion_dias", nullable = false)
+    private Integer duracionDias;
+
+    @Column(
+            nullable = false,
+            precision = 10,
+            scale = 2
+    )
+    private BigDecimal precio;
+
+    @Column(nullable = false)
+    private Boolean activo = true;
 }
