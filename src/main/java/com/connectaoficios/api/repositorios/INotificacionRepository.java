0746e74b1ff14e6bfdb3a7870a1221e068ef1134
karla <karlarosauras@gmail.com>
2026-09-22 22:22:31 -0600

    feat(repo): [CON-118] Karla Rosaura Sánchez Hernández- Creación de Repositorio de Notificacion

---
+++ src/main/java/com/connectaoficios/api/repositorios/INotificacionRepository.java
@@ -0,0 +1,24 @@
+package com.connectaoficios.api.repositorios;
+
+import com.connectaoficios.api.modelos.Notificacion;
+import com.connectaoficios.api.enums.TipoNotificacion;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface INotificacionRepository extends JpaRepository<Notificacion, Long> {
+
+    List<Notificacion> findByUsuarioDestinoId(Integer usuarioDestinoId);
+
+    Page<Notificacion> findByUsuarioDestinoId(Integer usuarioDestinoId, Pageable pageable);
+
+    List<Notificacion> findByUsuarioDestinoIdAndLeida(Integer usuarioDestinoId, Boolean leida);
+
+    List<Notificacion> findByTipo(TipoNotificacion tipo);
+
+    long countByUsuarioDestinoIdAndLeidaFalse(Integer usuarioDestinoId);
+}
