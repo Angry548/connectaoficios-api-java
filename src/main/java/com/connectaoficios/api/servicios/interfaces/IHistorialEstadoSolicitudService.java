80ce4000ffeb21b05ed08ea144d480cfa15823bc
karla <karlarosauras@gmail.com>
2026-09-23 09:11:21 -0600

    feat(Service): [CON-161] Karla Rosaura Sánchez Hernández- Creacion de la Interfaz de HistorialEstadoSolicitud

---
+++ src/main/java/com/connectaoficios/api/servicios/interfaces/IHistorialEstadoSolicitudService.java
@@ -0,0 +1,27 @@
+package com.connectaoficios.api.servicios.interfaces;
+
+import com.connectaoficios.api.dtos.historial.HistorialEstadoSolicitudGuardar;
+import com.connectaoficios.api.dtos.historial.HistorialEstadoSolicitudRespuesta;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface IHistorialEstadoSolicitudService {
+
+    List<HistorialEstadoSolicitudRespuesta> obtenerTodos();
+
+    HistorialEstadoSolicitudRespuesta obtenerPorId(Long id);
+
+    Page<HistorialEstadoSolicitudRespuesta> obtenerTodosPaginados(Pageable pageable);
+
+    List<HistorialEstadoSolicitudRespuesta> obtenerPorSolicitud(Long solicitudId);
+
+    Page<HistorialEstadoSolicitudRespuesta> obtenerPorSolicitudPaginado(Long solicitudId, Pageable pageable);
+
+    HistorialEstadoSolicitudRespuesta guardar(HistorialEstadoSolicitudGuardar historialGuardar);
+
+    void eliminar(Long id);
+}
+
+
