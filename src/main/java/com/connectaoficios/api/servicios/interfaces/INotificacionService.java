eeb92b8a0f83e009bff71d456a364cdec2f603c5
karla <karlarosauras@gmail.com>
2026-09-22 22:53:52 -0600

    feat(service): [CON-120] Karla Rosaura Sánchez Hernández- Creación de la Interfaz de Notificacion

---
+++ src/main/java/com/connectaoficios/api/servicios/interfaces/INotificacionService.java
@@ -0,0 +1,27 @@
+package com.connectaoficios.api.servicios.interfaces;
+
+import com.connectaoficios.api.dtos.notificacion.NotificacionGuardar;
+import com.connectaoficios.api.dtos.notificacion.NotificacionRespuesta;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface INotificacionService {
+
+    List<NotificacionRespuesta> obtenerTodas();
+
+    NotificacionRespuesta obtenerPorId(Long id);
+
+    Page<NotificacionRespuesta> obtenerTodasPaginadas(Pageable pageable);
+
+    List<NotificacionRespuesta> obtenerPorUsuario(Integer usuarioId);
+
+    Page<NotificacionRespuesta> obtenerPorUsuarioPaginado(Integer usuarioId, Pageable pageable);
+
+    NotificacionRespuesta guardar(NotificacionGuardar notificacionGuardar);
+
+    NotificacionRespuesta marcarComoLeida(Long id);
+
+    void eliminar(Long id);
+}
